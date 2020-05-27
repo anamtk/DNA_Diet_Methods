6da8172b8d25a7dacd59d26e0ce152464a90d356
Ana <ana.miller.ter.kuile@gmail.com>
2020-05-27 13:44:56 -0500

    creating a script that just sorts rarefied data by taxonomy without any statistical analysis. outputs can be used in statistics

--- 5_statistical_analyses/rarefied_taxonomically_sorted.R
+++ 5_statistical_analyses/rarefied_taxonomically_sorted.R
@@ -1,42 +1,17 @@
 ########################
 # Ana Miller-ter Kuile
-#Potential prey detection analyses ####
+#Rarefied taxonomically sorted ####
 # May 27, 2020
 ########################
 
-#These code look at how surface sterilization treatment changes our ability to
-#detect potential prey items for consumers we offered a potential prey item
-#in mesocosms and for consumers which were free to roam in the field and feed
-#on potential prey (and come into contact with potential environmental contaminants)
-#in a natural way.
+#These code create dataframes of the different "kinds" of DNA in each sample for
+#each environment. This includes known prey (for mesocosms), all prey, and predator
+#DNA (for both mesocosm and field predators)
 
 ##########################
 # Required packages ####
 library(here) #tidy data
 library(tidyverse) #tidy data
-library(ggplot2) #visualize data
-#library(emmeans) #marginal means of models
-#library(rcompanion) #pairwise comparisions
-#library(effects) #dotplots and stuff
-#library(lattice) #again dotplots
-library(DHARMa) #model diagnostics
-library(MuMIn) #model diagnostics
-library(glmmTMB) #mixed models
-library(lme4) #mixed models
-#library(ggeffects) #ggpredict function
-library(performance) #for binomail model fit
-library(see) #for binomial model fit
-#library(coin) #wilcoxon test
-#library(MASS) #glm.nb
-#library(vegan) #rrarefy
-library(cowplot) #plot grid at end
-#library(picante) #phylogenetic analyses
-#library(ape) #phylogenetic analyses
-#library(phytools) #phylogenetic analyses
-#library(phyloseq) #phylogenetic analyses
-#library(hciR) #for as_matrix function
-#library(esc) #effect sizes
-#library(effsize) #alternative effect size package
 ###########################
 
 #We will be using our taxonomic assignment dataframe created in the 
@@ -62,13 +37,13 @@ metadata <- read.csv(here("data", "Sample_Metadata.csv"))
 taxonomies <- read.csv(here("data", "outputs", "taxonomic_assignments.csv"))
 
 ##########################
-# Mesocosms: Combine and tidy dataframes for analysis ####
+# Mesocosms: Known Prey ####
 ##########################
 
 #bind community matrix to taxonomies, subset prey item,
 #gather dataframe so it's ready for a glm analysis, and 
 #attach metadata on surface sterilization treatments
-lab_rare <- lab_rare %>%
+lab_known_prey <- lab_rare %>%
   rename("ASV" = "X") %>%
   left_join(taxonomies, by = "ASV") %>% #combine with taxonomy DF
   filter(ID_ncbi == "Acrididae") %>% #select known diet only
@@ -79,11 +54,60 @@ lab_rare <- lab_rare %>%
   summarise(reads = sum(reads)) %>% #summarise total prey reads by sample 
   left_join(metadata, by = "sample") #attach metadata on sterilization treatment
  
+
+##########################
+# Mesocosms: All Prey ####
+##########################
+lab_prey <- lab_rare %>% 
+  rename("ASV" = "X") %>%
+  left_join(taxonomies, by = "ASV") %>% #join by taxonomy
+  filter(taxonomy == "prey") #filter out just the prey taxonomies
+
+#make the ID columns characters for the ifelse statements below
+field_prey$ID_bold <- as.character(field_prey$ID_bold)
+field_prey$ID_ncbi <- as.character(field_prey$ID_ncbi)
+
+#ifelse statement that assigns each ASV to a specific taxonomy
+#I want the BOLD ids to be the last option because these are to
+#species in many cases where NCBI/Genbank blasted to order, family, or genus
+#therefore, I'm going to say that if there is no BOLD id, give it the NCBI
+#ID, otherwise, give it the BOLD ID.
+unique_ID <- ifelse(is.na(field_prey$ID_bold), field_prey$ID_ncbi, 
+                    field_prey$ID_bold)
+
+#look at what these are:
+unique_ID
+
+#make this a dataframe so we can attach it back to the community
+#matrix for analyses at the sample level and later Jaccard dissimiliarity
+unique_ID <- as.data.frame(unique_ID)
+unique_ID$ASV <- field_prey$ASV #give it an ASV column for attaching later
+
+field_prey <- field_prey %>%
+  left_join(unique_ID, by = "ASV")  %>%
+  gather(sample, reads, HEV65:HEV100) %>%
+  group_by(sample, unique_ID) %>%
+  summarise(reads = sum(reads)) %>%
+  left_join(metadata, by = "sample")
+
+#Some of these have zero counts across all samples, so I'd like to delete
+#them for later. 
+
+fld_zeros <- field_prey %>%
+  ungroup() %>%
+  group_by(unique_ID) %>% #group by unique_ID
+  summarise(reads = sum(reads)) %>% #sum across all samples
+  filter(reads == 0) %>% #find the ones that are equal to zero
+  dplyr::select(unique_ID) #create a dataframe of just their names
+
+field_prey <- field_prey %>%
+  anti_join(fld_zeros, by = "unique_ID") #anti join to remove the IDs that are zero across all smaples
+
 ##########################
 # Field: Combine and tidy dataframes for analysis ####
 ##########################
 
-field_rare <- field_rare %>% 
+field_prey <- field_rare %>% 
   rename("ASV" = "X") %>%
   left_join(taxonomies, by = "ASV") %>% #join by taxonomy
   filter(taxonomy == "prey") #filter out just the prey taxonomies
@@ -92,16 +116,16 @@ field_rare <- field_rare %>%
 #concatenate before analyses
 
 #make the ID columns characters for the ifelse statements below
-field_rare$ID_bold <- as.character(field_rare$ID_bold)
-field_rare$ID_ncbi <- as.character(field_rare$ID_ncbi)
+field_prey$ID_bold <- as.character(field_prey$ID_bold)
+field_prey$ID_ncbi <- as.character(field_prey$ID_ncbi)
 
 #ifelse statement that assigns each ASV to a specific taxonomy
 #I want the BOLD ids to be the last option because these are to
 #species in many cases where NCBI/Genbank blasted to order, family, or genus
 #therefore, I'm going to say that if there is no BOLD id, give it the NCBI
 #ID, otherwise, give it the BOLD ID.
-unique_ID <- ifelse(is.na(field_rare$ID_bold), field_rare$ID_ncbi, 
-                 field_rare$ID_bold)
+unique_ID <- ifelse(is.na(field_prey$ID_bold), field_prey$ID_ncbi, 
+                 field_prey$ID_bold)
 
 #look at what these are:
 unique_ID
@@ -109,9 +133,9 @@ unique_ID
 #make this a dataframe so we can attach it back to the community
 #matrix for analyses at the sample level and later Jaccard dissimiliarity
 unique_ID <- as.data.frame(unique_ID)
-unique_ID$ASV <- field_rare$ASV #give it an ASV column for attaching later
+unique_ID$ASV <- field_prey$ASV #give it an ASV column for attaching later
 
-field_rare <- field_rare %>%
+field_prey <- field_prey %>%
   left_join(unique_ID, by = "ASV")  %>%
   gather(sample, reads, HEV65:HEV100) %>%
   group_by(sample, unique_ID) %>%
@@ -121,12 +145,12 @@ field_rare <- field_rare %>%
 #Some of these have zero counts across all samples, so I'd like to delete
 #them for later. 
 
-fld_zeros <- field_rare %>%
+fld_zeros <- field_prey %>%
   ungroup() %>%
   group_by(unique_ID) %>% #group by unique_ID
   summarise(reads = sum(reads)) %>% #sum across all samples
   filter(reads == 0) %>% #find the ones that are equal to zero
   dplyr::select(unique_ID) #create a dataframe of just their names
 
-field_rare <- field_rare %>%
+field_prey <- field_prey %>%
   anti_join(fld_zeros, by = "unique_ID") #anti join to remove the IDs that are zero across all smaples
