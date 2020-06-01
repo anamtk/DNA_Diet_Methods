729370cd92c307cedb288e2ca21f03f1dbc655fa
Ana <ana.miller.ter.kuile@gmail.com>
2020-06-01 16:51:25 -0500

    updated visuals for abundance analysis, including supplementary figures

--- 5_tidying_and_statistics/5_abundance_analyses.R
+++ 5_tidying_and_statistics/5_abundance_analyses.R
@@ -165,17 +165,65 @@ zi <- testZeroInflation(simulationOutput)
 od <- testDispersion(simulationOutput)
 
 ###########################
-# Lab Predator model ####
+# Visualizations ####
+############################
+#let's visualize this then
+my_y_title_2 <- expression(paste("Percent ", italic("O. japonica"), " reads per sample"))
+
+(b_1 <- ggplot(lab_all_nz, aes(x = Sterilized, y = (known/total)*100)) +
+  geom_boxplot(fill = "#7B8D65") +
+  theme_bw() + theme(legend.position = "none") +
+  labs(x = "Sterilization treatment", y = my_y_title_2) +
+  scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized"))) 
+
+(b_2 <- ggplot(lab_all_nz, aes(x = Sterilized, y = (known/total)*100)) +
+  geom_boxplot(fill = "#7B8D65") +
+  scale_y_log10() +
+  theme_bw() + theme(legend.position = "none") +
+  labs(x = "Sterilization treatment", y = my_y_title_2) +
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank()))
+
+(e_1 <- ggplot(fld_all_nz, aes(x = Sterilized, y = (prey/total)*100)) +
+  geom_boxplot(fill = "#F29979") +
+  labs(x = "Surface sterilization treatment", y = "Percent prey reads per sample") +
+  theme_bw() + theme(legend.position = "none") +
+  scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized")))
+
+(e_2 <- ggplot(fld_all_nz, aes(x = Sterilized, y = (prey/total)*100)) +
+  geom_boxplot(fill = "#F29979") +
+  scale_y_log10()+
+  labs(x = "Surface sterilization treatment", y = "Percent prey reads per sample") +
+  theme_bw() + theme(legend.position = "none") +
+  scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized")))
+
+###########################
+# Plot grid and summaries for paper ####
 ############################
 
-#These models are not nice...
+plot_grid(b_2, e_2, nrow = 2, align = "vh")
+lab_all_nz %>%
+  summarise(prop = known/total) %>%
+  summarise(mean = mean(prop), total= n(), sd = sd(prop), se = sd/sqrt(total))
 
-lab_pred_mod <- glmmTMB(log10(pred) ~ Sterilized,
+fld_all_nz %>%
+  summarise(prop = prey/total) %>%
+  summarise(mean = mean(prop), total= n(), sd = sd(prop), se = sd/sqrt(total))
+
+###########################
+# Supplement: Lab Predator model ####
+############################
+
+#These predator models are not nice...
+#Basically, what I'm trying to show here is that there is no "relic" that would
+#be driving the marginal difference in the mesocosm predator known prey reduction
+#but rather this represents some effect of surface contamination. So yeah...
+
+lab_pred_mod <- glmmTMB(pred ~ Sterilized,
                    data = lab_all,
                    REML = FALSE,
                    family = "gaussian")
 
-lab_pred_null <- glmmTMB(log10(pred) ~ 1,
+lab_pred_null <- glmmTMB(pred ~ 1,
                     data = lab_all,
                     REML = FALSE,
                     family = "gaussian")
@@ -197,9 +245,8 @@ plot(residuals(lab_pred_null))
 simulationOutput <- simulateResiduals(fittedModel = lab_pred_null) 
 fit <- plot(simulationOutput, asFactor=TRUE)
 
-
 ###########################
-# Field Predator model ####
+# Supplement: Field Predator model ####
 ############################
 fld_pred_mod <- glmmTMB(pred ~ Sterilized,
                         data = fld_all,
@@ -234,14 +281,79 @@ fit <- plot(simulationOutput, asFactor=TRUE)
 zi <- testZeroInflation(simulationOutput) 
 od <- testDispersion(simulationOutput)
 
-ggplot(fld_all, aes(x = Sterilized, y = prey/total)) +
-  geom_boxplot() + theme_bw() +scale_y_log10()
 
-ggplot(lab_all, aes(x = Sterilized, y = known/total)) +
-  geom_boxplot() + theme_bw() +scale_y_log10()
+###########################
+# Supplement: Lab all Prey model ####
+############################
 
-ggplot(fld_all, aes(x = Sterilized, y = pred/total)) +
-  geom_boxplot() + theme_bw() 
+lab_all_prey <- lab_all %>%
+  filter(prey > 0)
 
-ggplot(lab_all, aes(x = Sterilized, y = pred/total)) +
-  geom_boxplot() + theme_bw() 
+lab_all_mod <- glmmTMB(prey ~ Sterilized,
+                   data = lab_all_prey,
+                   offset = log(total),
+                   REML = FALSE,
+                   family = "genpois")
+
+lab_all_null <- glmmTMB(prey ~ 1,
+                    data = lab_all_prey,
+                    offset = log(total),
+                    REML = FALSE,
+                    family = "genpois")
+
+AICc(lab_mod, lab_null)
+
+lab_all_mod <- glmmTMB(prey ~ Sterilized,
+                   data = lab_all_prey,
+                   offset = log(total),
+                   family = "genpois")
+
+lab_all_null <- glmmTMB(prey ~ 1,
+                    data = lab_all_prey,
+                    offset = log(total),
+                    family = "genpois")
+
+summary(lab_all_mod)
+
+plot(allEffects(lab_all_mod))
+
+plot(residuals(lab_all_null))
+simulationOutput <- simulateResiduals(fittedModel = lab_all_null) 
+fit <- plot(simulationOutput, asFactor=TRUE)
+zi <- testZeroInflation(simulationOutput) 
+od <- testDispersion(simulationOutput)
+
+###########################
+# Supplement: Predator and other prey graphs ####
+############################
+(lab_pred_graph <- ggplot(lab_all, aes(x = Sterilized, y = pred)) +
+   geom_boxplot(fill = "#7B8D65") + theme_bw() + 
+    labs(x = "Surface sterilization treatment", y = "Consumer DNA reads") +
+    scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized")) + 
+    theme(legend.position = "none")
+  )
+
+(fld_pred_graph <- ggplot(fld_all, aes(x = Sterilized, y = pred)) +
+    geom_boxplot(fill = "#F29979") + theme_bw() + 
+    labs(x = "Surface sterilization treatment", y = "Consumer DNA reads") +
+    scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized")) + 
+    theme(legend.position = "none")
+)
+
+(lab_prey_graph <- ggplot(lab_all_prey, aes(x = Sterilized, y = prey)) +
+    geom_boxplot(fill = "#7B8D65") + theme_bw() + 
+    scale_y_log10() +
+    labs(x = "Surface sterilization treatment", y = "Potential diet DNA reads") +
+    scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized")) + 
+    theme(legend.position = "none")
+)
+
+(fld_prey_graph <- ggplot(fld_all_nz, aes(x = Sterilized, y = prey)) +
+    geom_boxplot(fill = "#F29979") +
+    scale_y_log10()+
+    labs(x = "Surface sterilization treatment", y = "Potential diet DNA reads") +
+    theme_bw() + theme(legend.position = "none") +
+    scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized")))
+
+plot_grid(lab_pred_graph, fld_pred_graph, lab_prey_graph, fld_prey_graph, 
+          ncol = 2, align = "vh")
