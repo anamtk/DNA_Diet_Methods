8655a39b56c9fd0fa01dacdb2b7594e6909d711d
Ana <ana.miller.ter.kuile@gmail.com>
2020-06-01 14:56:35 -0500

    added a dataframe of samples that successfully amplified vs not

---
+++ 5_tidying_and_statistics/amplification_success.R
@@ -0,0 +1,17 @@
+###########################
+# Amplification success####
+# Ana Miller-ter Kuile
+# June 1, 2020
+############################
+library(here)
+library(tidyverse)
+amp <- read.csv(here("data", "Amplification_Success.csv"))
+metadata <- read.csv(here("data", "Sample_Metadata.csv"))
+
+amp %>%
+  left_join(metadata, by = c("sample", "Source", "Sterilized")) %>%
+  group_by(Source, Sterilized) %>%
+  summarise(total = n(), proportion = sum(Amp_success/n()), success = sum(Amp_success >0))
+
+amp %>%
+  summarise(total = n(), proportion = sum(Amp_success/n()), success = sum(Amp_success >0))
