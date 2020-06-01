237469a94004fb1df2ee2f7ab45e95091aef1ca6
Ana <ana.miller.ter.kuile@gmail.com>
2020-06-01 16:55:18 -0500

    updated file path for csv file after organization of data folder

--- 4_seq_depth_and_error/asv_table_cleaning.R
+++ 4_seq_depth_and_error/asv_table_cleaning.R
@@ -12,7 +12,7 @@ library(DHARMa)
 #using the method from Chris Jerde in the eDNA workshop
 
 #load the dataframe
-u3_error_check <- read.csv(here("data", "ASV_tables", "unoise_uc_zotu_tab.txt"), sep = "\t")
+u3_error_check <- read.csv(here("data", "denoised_data", "ASV_tables", "unoise_uc_zotu_tab.txt"), sep = "\t")
 #rename columns for simplicity
 colnames(u3_error_check) <- sapply(str_split(colnames(u3_error_check), "S"), function(x){return(x[[1]])})
 u3_error_check <- rename(u3_error_check, "ASV" = "X.OTU.ID")
