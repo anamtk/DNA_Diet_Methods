bc6146262464e9ba05ae6ea5923b7c454e7b5098
Ana <ana.miller.ter.kuile@gmail.com>
2020-12-04 12:31:18 -0600

    broke up richness and composition scripts and added ASV-level analysis to both as well

--- 6_statistics/3_field_composition.R
+++ 6_statistics/3_field_composition.R
@@ -14,81 +14,35 @@
 # Required packages ####
 ############################
 
-library(here) #tidy data
-library(tidyverse) #tidy data
-library(ggplot2) #visualize data
-library(DHARMa) #model diagnostics
-library(MuMIn) #model diagnostics
-library(glmmTMB) #mixed models
-library(performance) #for binomail model fit
-library(vegan) #adonis
-library(esc) #effect sizes
+package.list <- c("here", "tidyverse", 
+                  "glmmTMB", "MuMIn", 
+                  "DHARMa", "performance",
+                  "vegan", "remotes",
+                  "eulerr", "dummies",
+                  "esc")
+
+## Installing them if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## And loading them
+for(i in package.list){library(i, character.only = T)}
+
 
 ###########################
 # Load data ####
 ############################
 
-field <- read.csv(here("data", "outputs", "rarefied_taxonomic_sort", "field_prey_rare.csv"))
+field <- read.csv(here("data", 
+                       "outputs", 
+                       "rarefied_taxonomic_sort", 
+                       "field_prey_rare.csv"))
 
-###########################
-# Field Richness analysis ####
-############################
+ASV <- read.csv(here("data", 
+                     "outputs", 
+                     "rarefied_taxonomic_sort", 
+                     "field_prey_ASVs_rare.csv"))
 
-richness <- field %>%
-  group_by(sample, Sterilized) %>%
-  summarise(SR = sum(reads > 0, na.rm=TRUE)) #number of species with greater than 0 reads
-
-#create a model with and without surface sterilization effect, with poisson
-#distribution since these are species count data
-#set REML to false so that we can do AICc comparision
-rich_mod <- glmmTMB(SR ~ Sterilized,
-                    data = richness, 
-                    family = poisson,
-                    REML = FALSE)
-
-rich_null <- glmmTMB(SR ~ 1,
-                    data = richness, 
-                    family = poisson, 
-                    REML = FALSE)
-
-#compare them with AIC, showing that the null is a better fit
-AICc(rich_mod, rich_null)
-
-#re-fit with REML
-rich_null <- glmmTMB(SR ~ 1,
-                     data = richness, 
-                     family = poisson)
-
-rich_mod <- glmmTMB(SR ~ Sterilized,
-                    data = richness, 
-                    family = poisson)
-
-#based on this summary, surface sterilizatoin treatment is 
-#non-significant. 
-summary(rich_mod)
-
-summary(rich_null)
-#these diagnostics look fine!
-plot(residuals(rich_null))
-simulationOutput <- simulateResiduals(fittedModel = rich_null) 
-fit <- plot(simulationOutput, asFactor=TRUE)
-zi <- testZeroInflation(simulationOutput) 
-od <- testDispersion(simulationOutput) 
-
-###########################
-# Plot for paper of richness ####
-############################
-(rich_graph <- ggplot(richness, aes(x = Sterilized, y = SR)) +
-   geom_boxplot(fill = "#F29979") + theme_bw() +
-   labs(x = "Surface sterilization treatment", y = "Prey family richness") +
-   scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized")) +
-   theme(legend.position = "none"))
-
-richness %>%
-  ungroup() %>%
-  summarise(mean= mean(SR), total = n(), sd = sd(SR), se = sd/sqrt(total))
-max(richness$SR)
-min(richness$SR)
 ###########################
 # Field presence-absence composition analysis (PERMANOVA) ####
 ############################
@@ -128,83 +82,6 @@ binned_residuals(comp_null)
 simulationOutput <- simulateResiduals(fittedModel = comp_null)
 fit <- plot(simulationOutput, asFactor=TRUE)
 
-###########################
-# Heat map visualization of both presence and abundance ####
-#(At order level)
-############################
-heat <- field
-
-#make order and ID values characters for ifelse sorting to order level below
-heat$Order_ncbi <- as.character(heat$Order_ncbi)
-
-#ifelse statements creating an overall "Order" column for visualization
-heat$Order <- heat$Order_ncbi
-
-#now we can summarise heat by order and sterilization treatment for
-#the heatmap graph
-heat_order <- heat %>%
-  group_by(Order, Sterilized) %>% #group by order and sterilization treatment
-  summarise(reads = mean(reads)) %>% #then find the mean abundance for each order
-  ungroup() %>% #ungroup so we can make order a factor
-  mutate(Order = as.factor(Order)) %>% #make order a factor
-  pivot_wider(names_from = Sterilized, values_from = reads) %>% #make wide for two columns
-  #for sterilization treatment so we can arrange in descending order of abundance
-  arrange((NS + SS), (NS)) %>% #arrange in descending order of abundance
-  mutate(Order = factor(Order, levels = Order)) %>% #reset the levels of order on this
-  #new abundance-based ordering of the Order factor
-  gather(Sterilized, reads, NS:SS) #gather DF back up for visualization
-
-#Because read abundance has such a broad range, we want to set a quantile-based
-#variable for these abundances for the color ramp in the graph, but also want
-#to discount the effects of zero reads in this, so we will create a DF of non-zero
-#values for reads, and then find quantiles of this to inform our quantiles for the figure
-heat_order_nz <- heat_order %>%
-  filter(reads > 0)
-quantile(heat_order_nz$reads)
-#0.2777778  0.7400097   2.5789474   9.4725877 120.7222222 
-
-#now we can set a quantile variable in our DF
-heat_order$quantiles <- ifelse(heat_order$reads == 0, 0,
-                             ifelse(heat_order$reads > 0 & heat_order$reads <= 0.2777778, 1, 
-                                    ifelse(heat_order$reads > 0.2777778 & heat_order$reads <= 0.7400097, 2,
-                                           ifelse(heat_order$reads > 0.7400097 & heat_order$reads <= 2.5789474, 3,
-                                                  ifelse(heat_order$reads > 2.5789474 & heat_order$reads <= 9.4725877, 4, 5)))))
-
-#making it a factor for visualization
-heat_order$quantiles <- as.factor(heat_order$quantiles)  
-
-#these are two color palettes that could be used in this figure
-pal <- c(
-  '0' = "#FFFFFF",
-  '1' = "#F27D72", 
-  '2' = "#D26F67", 
-  '3' = "#B2615C",
-  '4' = "#925451",
-  '5' = "#734646"
-)
-
-pal2 <- c(
-  '0' = "#FFFFFF",
-  '1' = "#F29979", 
-  '2' = "#D2846C", 
-  '3' = "#B26F5F",
-  '4' = "#925B53",
-  '5' = "#734646"
-)
-
-(heat_map_order <- ggplot(heat_order, aes(x = Sterilized, y = Order, fill=quantiles, height = 0.95, width = 0.95)) +
-  geom_tile() + 
-  coord_equal() +
-  labs(x = "Surface sterilization treatment", y = "Diet order") +
-  scale_x_discrete(labels=c("NS" = "Not Sterilized", "SS" = "Surface Sterilized")) +
-  scale_fill_manual(name = "Mean read abundance\n(divided by quantiles)",
-                    values = pal2,
-                    limits = names(pal2),
-                    labels = c("0", "< 0.28", "< 0.74", "< 2.58", "< 9.47", "> 9.47")) + 
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)))
-
-#0.2222222   0.5031676   2.8961988   9.7087719 120.5555556 
 
 ###########################
 # Heat map visualization of both presence and abundance ####
@@ -281,6 +158,158 @@ pal2 <- c(
     theme(axis.text.x = element_text(angle = 45, hjust = 1)))
 
 ##0.05263158   0.37353801   1.32602339   6.77631579 220.05263158 
+
+
+###########################
+# Field presence-absence composition analysis CCA ####
+############################
+
+# Matrix for CCA -----------------------------------------------
+zeros <- ASV %>%
+  group_by(sample) %>%
+  summarise(reads = sum(reads)) %>%
+  filter(reads == 0)
+
+ASV <- ASV %>%
+  anti_join(zeros, by = "sample")
+
+#matrix with diet as columns, samples as rows
+mat <- ASV %>%
+  dplyr::select(sample, reads, ASV) %>%
+  pivot_wider(names_from = ASV,
+              values_from = reads,
+              values_fill = 0) %>% #fill missing values with 0's
+  column_to_rownames(var = "sample")
+
+mat_pres <- ASV %>%
+  dplyr::select(sample, reads, ASV) %>%
+  mutate(presence = ifelse(reads>0, 1, 0)) %>%
+  dplyr::select(-reads) %>%
+  pivot_wider(names_from = ASV,
+              values_from = presence,
+              values_fill = 0) %>% #fill missing values with 0's
+  column_to_rownames(var = "sample")
+
+#metadata
+meta <- ASV %>%
+  dplyr::select(sample, Sterilized) %>%
+  distinct(sample, Sterilized) %>%
+  column_to_rownames(var = "sample")
+
+#check that rownames are the same:
+all.equal(rownames(mat), rownames(meta))
+
+#check that rownames are the same:
+all.equal(rownames(mat_pres), rownames(meta))
+
+
+# CCA -----------------------------------------------------------
+#working off this tutorial
+#http://dmcglinn.github.io/quant_methods/lessons/multivariate_models.html
+# vegan requires that we write out each term if we are not going to 
+# convert the factor to a dummy matrix 
+# alternatively we could use a shorthand approach
+cca <-  cca(mat_pres ~ . , data=meta)
+
+#view the CCA loadings
+cca
+
+summary(cca)
+#how much variation explained in this RDA
+RsquareAdj(cca)
+#ANOVA of whole model
+anova(cca, permutations=10000)
+#ANOVA of model terms
+anova(cca, by='margin', permutations=10000)
+
+
+###########################
+# (OPTIONAL: adonis instead of GLMM for field presence) ####
+############################
+#Now we can run adonis, which asks whether there are differences in community
+#composition of diet species between the two treatment levels of sterilization
+#dist = jaccard with binary = TRUE because this is presence-absence data
+adonis(mat_pres ~ Sterilized, data = meta, dist = "jaccard", binary = TRUE)
+#there is no significnat difference between treatment groups 
+
+
+# Heat Map of ASVs ----------------------------------------------------------------
+heat <- ASV
+
+heat_nz <- heat %>%
+  filter(reads > 0)
+quantile(heat_nz$reads)
+#1    1    5   20.5 4079  
+
+#now we can summarise heat by order and sterilization treatment for
+#the heatmap graph
+ASV_ord <- heat %>%
+  group_by(ASV) %>%
+  summarise(reads = sum(reads)) %>%
+  arrange(reads) %>%
+  mutate(ASV = factor(ASV, levels = ASV)) %>%
+  dplyr::select(ASV) %>%
+  as_vector
+
+#now we can set a quantile variable in our DF
+ASV %>%
+  dplyr::select(sample, Sterilized) %>%
+  unique()
+
+heat$sample <- factor(heat$sample, levels = c("HEV100", "HEV106", "HEV107", "HEV108",
+                                              "HEV109", "HEV110", "HEV111", "HEV79",
+                                              "HEV81", "HEV82", "HEV83", "HEV87",
+                                              "HEV88", "HEV89", "HEV95", "HEV96", 
+                                              "HEV97", "HEV98", "HEV99", "HEV101",
+                                              "HEV102", "HEV103", "HEV104", "HEV105",
+                                              "HEV65", "HEV66", "HEV67", "HEV68", "HEV70",
+                                              "HEV71", "HEV74", "HEV76", "HEV90", 
+                                              "HEV91", "HEV92", "HEV93", "HEV94"))
+heat$quantiles <- ifelse(heat$reads == 0, 0,
+                         ifelse(heat$reads > 0 & heat$reads <= 1, 1, 
+                                ifelse(heat$reads > 1 & heat$reads <= 5, 3,
+                                       ifelse(heat$reads > 6 & heat$reads <= 20.5, 4, 5))))
+
+#making it a factor for visualization
+heat$quantiles <- as.factor(heat$quantiles) 
+
+#these are two color palettes that could be used in this figure
+pal3 <- c(
+  '0' = "#FFFFFF",
+  '1' = "#F27D72", 
+  '3' = "#D26F67", 
+  '4' = "#B2615C",
+  '5' = "#925451"
+)
+
+heat <- heat %>%
+  mutate(ASV = as.factor(ASV)) %>%
+  mutate(ASV = factor(ASV, levels = ASV_ord))
+
+(heat_map <- ggplot(heat, aes(x = sample, y = ASV, fill=quantiles)) +
+    geom_tile(color = "black") + 
+    coord_equal() +
+    labs(x = "Sample", y = "ASV") +
+    scale_fill_manual(name = "Mean read abundance\n(divided by quantiles)",
+                      values = pal3,
+                      limits = names(pal3),
+                      labels = c("0", "< 1", "< 5", "< 20", "> 20")) + 
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    geom_vline(xintercept = 17.5))
+
+
+
+
+# For paper - plots -------------------------------------------------------
+
+
+
+
+
+
+
+
 
 ###########################
 # Per-sample heat map as an alternative to effects map? ####
---
+++ 6_statistics/3_field_richness.R
@@ -0,0 +1,171 @@
+###########################
+# FIeld Richness Analyses of ASVs ####
+# Ana Miller-ter Kuile
+# May 27, 2020 
+############################
+
+# This script is looks at ASV richness for field predators, and then
+#looks at species composition of this richness using  CCA analysis
+
+# Ending is additional analyses based on abundance for field, and including
+#presence-absence and abundance-based PERMANOVAs for the mesocosm spiders as well
+
+###########################
+# Required packages ####
+############################
+
+# Load packages -----------------------------------------------------------
+
+package.list <- c("here", "tidyverse", 
+                  "glmmTMB", "MuMIn", 
+                  "DHARMa", "performance",
+                  "vegan", "remotes",
+                  "eulerr", "dummies",
+                  "esc")
+
+## Installing them if they aren't already on the computer
+new.packages <- package.list[!(package.list %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## And loading them
+for(i in package.list){library(i, character.only = T)}
+
+###########################
+# Load data ####
+############################
+
+field <- read.csv(here("data",
+                       "outputs", 
+                       "rarefied_taxonomic_sort", 
+                       "field_prey_rare.csv"))
+
+
+ASV <- read.csv(here("data", 
+                     "outputs", 
+                     "rarefied_taxonomic_sort", 
+                     "field_prey_ASVs_rare.csv"))
+
+
+###########################
+# Field Richness analysis ####
+############################
+
+richness <- field %>%
+  group_by(sample, Sterilized) %>%
+  summarise(SR = sum(reads > 0, na.rm=TRUE)) #number of species with greater than 0 reads
+
+#create a model with and without surface sterilization effect, with poisson
+#distribution since these are species count data
+#set REML to false so that we can do AICc comparision
+rich_mod <- glmmTMB(SR ~ Sterilized,
+                    data = richness, 
+                    family = poisson,
+                    REML = FALSE)
+
+rich_null <- glmmTMB(SR ~ 1,
+                     data = richness, 
+                     family = poisson, 
+                     REML = FALSE)
+
+#compare them with AIC, showing that the null is a better fit
+AICc(rich_mod, rich_null)
+
+#re-fit with REML
+rich_null <- glmmTMB(SR ~ 1,
+                     data = richness, 
+                     family = poisson)
+
+rich_mod <- glmmTMB(SR ~ Sterilized,
+                    data = richness, 
+                    family = poisson)
+
+#based on this summary, surface sterilizatoin treatment is 
+#non-significant. 
+summary(rich_mod)
+
+summary(rich_null)
+#these diagnostics look fine!
+plot(residuals(rich_null))
+simulationOutput <- simulateResiduals(fittedModel = rich_null) 
+fit <- plot(simulationOutput, asFactor=TRUE)
+zi <- testZeroInflation(simulationOutput) 
+od <- testDispersion(simulationOutput) 
+
+###########################
+# Plot for paper of richness ####
+############################
+(rich_graph <- ggplot(richness, aes(x = Sterilized, y = SR)) +
+   geom_boxplot(fill = "#F29979") + theme_bw() +
+   labs(x = "Surface sterilization treatment", y = "Prey family richness") +
+   scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized")) +
+   theme(legend.position = "none"))
+
+richness %>%
+  ungroup() %>%
+  summarise(mean= mean(SR), total = n(), sd = sd(SR), se = sd/sqrt(total))
+max(richness$SR)
+min(richness$SR)
+
+###########################
+# Field ASV Richness analysis ####
+############################
+
+richness_ASV <- ASV %>%
+  group_by(sample, Sterilized) %>%
+  summarise(SR = sum(reads > 0, na.rm=TRUE)) #number of ASVs with greater than 0 reads
+
+richness_ASV %>%
+  ungroup() %>%
+  summarise(rich = mean(SR),
+            max = max(SR),
+            se = sd(SR)/sqrt(n()))
+#create a model with and without surface sterilization effect, with poisson
+#distribution since these are species count data
+#set REML to false so that we can do AICc comparision
+rich_mod <- glmmTMB(SR ~ Sterilized,
+                    data = richness_ASV, 
+                    family = poisson,
+                    REML = FALSE)
+
+rich_null <- glmmTMB(SR ~ 1,
+                     data = richness_ASV, 
+                     family = poisson, 
+                     REML = FALSE)
+
+#compare them with AIC, showing that the null is a better fit
+AICc(rich_mod, rich_null)
+
+#re-fit with REML
+rich_null <- glmmTMB(SR ~ 1,
+                     data = richness, 
+                     family = poisson)
+
+rich_mod <- glmmTMB(SR ~ Sterilized,
+                    data = richness, 
+                    family = poisson)
+
+#based on this summary, surface sterilizatoin treatment is 
+#non-significant. 
+summary(rich_mod)
+
+summary(rich_null)
+#these diagnostics look fine!
+plot(residuals(rich_null))
+simulationOutput <- simulateResiduals(fittedModel = rich_null, plot= T) 
+testZeroInflation(simulationOutput) 
+testDispersion(simulationOutput) 
+
+###########################
+# Plot for paper of richness ####
+############################
+(ASV_rich_graph <- ggplot(richness_ASV, aes(x = Sterilized, y = SR)) +
+   geom_boxplot(fill = "#F29979") + theme_bw() +
+   labs(x = "Surface sterilization treatment", y = "ASV richness") +
+   scale_x_discrete(labels=c("NS" = "Not S. Sterilized", "SS" = "Surface Sterilized")) +
+   theme(legend.position = "none"))
+
+richness_ASV %>%
+  ungroup() %>%
+  summarise(mean= mean(SR), total = n(), sd = sd(SR), se = sd/sqrt(total))
+max(richness_ASV$SR)
+min(richness_ASV$SR)
